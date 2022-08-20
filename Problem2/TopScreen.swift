d45dd443fa3f0130b34eb551ad540cb99736d9a2
Ryoichi Izumita <r.izumita@caph.jp>
2022-08-20 17:27:48 +0900

    画面Bで入力した文字列をTop画面で表示

--- Problem2/TopScreen.swift
+++ Problem2/TopScreen.swift
@@ -9,15 +9,35 @@ import SwiftUI
 
 struct TopScreen: View {
     @State private var text: String = ""
+    @State private var textFromBScreen: String = ""
 
     var body: some View {
         NavigationView {
             VStack {
+                Spacer()
+
                 NavigationLink(destination: AScreen(text: text)) {
                     Text("画面Aを表示")
                 }
+
                 TextField("画面Aに渡す文字列を入力", text: $text)
+                .multilineTextAlignment(.center)
                 .padding()
+
+                Spacer()
+
+                NavigationLink(destination: BScreen(text: $textFromBScreen)) {
+                    Text("画面Bを表示")
+                }
+
+                Text("画面Bで入力した文字列")
+                .foregroundColor(Color.gray)
+                .padding()
+
+                Text(textFromBScreen)
+                .padding()
+
+                Spacer()
             }
             .navigationBarHidden(true)
             .ignoresSafeArea(.keyboard)
